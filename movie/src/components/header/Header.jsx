51b78d3f88ddb9d2b86599d7d9ef5ba5b4f35a6c
Yashi2305 <singhyashi.800@gmail.com>
2024-07-28 14:21:24 +0530

    fix

--- movie/src/components/header/Header.jsx
+++ movie/src/components/header/Header.jsx
@@ -12,7 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import logo from "../../assets/ll.png"
+import logo from 'movie/src/assets/11.png'
 
 const pages = ['Home', 'Series', 'Movies','Pricing','Pages','Contact'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
